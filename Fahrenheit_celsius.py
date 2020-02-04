d39a1d82ceeba4d6d96271a2ed5a99907edf7b0d
Azurick05 <andresgmorcillo@gmail.com>
2020-02-04 20:47:27 +0100

    Base del Programa

---
+++ Fahrenheit_celsius.py
@@ -0,0 +1,6 @@
+
+grados_fahrenheit = int(input("Introduzca la Temperatura en grados Fahrenheit: "))
+
+temperature_celsius = float((grados_fahrenheit - 32) / 1.8)
+
+print("La temperatura marcada en Fahrenheit ({}) pasa a ser {} grados celsius".format(grados_fahrenheit, temperature_celsius))
