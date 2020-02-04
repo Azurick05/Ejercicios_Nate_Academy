bf4c9a5fbe9680f2764f9f39e1150c80aa191a2a
Azurick05 <andresgmorcillo@gmail.com>
2020-02-04 20:34:19 +0100

    Incluyendo el contador

--- Adivinar_numero.py
+++ Adivinar_numero.py
@@ -2,11 +2,12 @@
 number_to_guess = int(input("Numero a adivinar del 0 al 10 (que tu comapañero no lo vea): "))
 
 insert_number = 100
+contador = 0
 
 while number_to_guess != insert_number:
     insert_number = int(input("Adivine el numero del 0 al 10: "))
-
+    contador += 1
     if insert_number != number_to_guess:
         print("Ha fallado, debe intentarlo de nuevo")
 
-print("Lo has adivinado!")
+print("Lo has adivinado y el numero de intentos ha sido: {}".format(contador))
