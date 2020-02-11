aff4a293d5e34c17359a1018f6b9411dfd2deae4
Azurick05 <andresgmorcillo@gmail.com>
2020-02-11 11:38:54 +0100

    Haciendo que el usuario meta el rango

--- Tabla_de_multiplicar_for.py
+++ Tabla_de_multiplicar_for.py
@@ -1,7 +1,9 @@
 
 numero_multiplicar = int(input("Introduzca el numero a multiplicar: "))
+primer_numero = int(input("Introduzca el primer numero del rango: "))
+segundo_numero = int(input("Introduzca el segundo numero del rango: "))
 
-for multiplo in range(5,16):
+for multiplo in range(primer_numero,segundo_numero + 1):
     print("{} x {} = {}".format(numero_multiplicar, multiplo, numero_multiplicar*multiplo))
 
 print("Se ha acabado la multiplicacion." )
