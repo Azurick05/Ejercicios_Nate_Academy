40296aa3b6e2b753c775d95eaf5d22cd63bf9124
Azurick05 <andresgmorcillo@gmail.com>
2020-02-13 22:37:33 +0100

    Mejorando el Programa

--- Tabla_de_multiplicar_while.py
+++ Tabla_de_multiplicar_while.py
@@ -2,11 +2,9 @@
 numero_a_multiplicar = int(input("Introduzca el numero a multiplicar: "))
 
 multiplicador = 5
-resultado = 0
 
 while multiplicador < 16:
-    resultado = multiplicador * numero_a_multiplicar
-    print("{} x {} : {}".format(numero_a_multiplicar, multiplicador, resultado))
+    print("{} x {} : {}".format(numero_a_multiplicar, multiplicador, numero_a_multiplicar*multiplicador))
     multiplicador += 1
 
 print("Ha terminaado la multiplicacion.")
